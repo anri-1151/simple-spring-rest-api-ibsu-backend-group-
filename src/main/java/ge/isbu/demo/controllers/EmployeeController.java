ad2de53f7dd8d950e887c461000f4cc4eedc9b8d
IBSU Lecturer <IBSU_Lecturer@ibsu.edu.ge>
2023-04-06 21:02:04 +0400

    add-update-check-props

--- src/main/java/ge/isbu/demo/controllers/EmployeeController.java
+++ src/main/java/ge/isbu/demo/controllers/EmployeeController.java
@@ -1,11 +1,13 @@
 package ge.isbu.demo.controllers;
 
+import ge.isbu.demo.Util.GeneralUtil;
 import ge.isbu.demo.dto.AddEmployee;
 import ge.isbu.demo.entities.Employee;
 import ge.isbu.demo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -27,7 +29,14 @@ public class EmployeeController {
 
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = {"application/json"})
     public Employee add(@RequestBody AddEmployee addEmployee) throws Exception {
+        GeneralUtil.checkRequiredProperties(addEmployee, Arrays.asList("firstName", "lastName", "departmentId"));
         return employeeService.add(addEmployee);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT, produces = {"application/json"})
+    public Employee edit(@PathVariable Long id, @RequestBody AddEmployee addEmployee) throws Exception {
+        GeneralUtil.checkRequiredProperties(addEmployee, Arrays.asList("firstName", "lastName", "departmentId"));
+        return employeeService.edit(id, addEmployee);
     }
 
 }
--- src/main/java/ge/isbu/demo/services/EmployeeService.java
+++ src/main/java/ge/isbu/demo/services/EmployeeService.java
@@ -1,9 +1,11 @@
 package ge.isbu.demo.services;
 
+import ge.isbu.demo.Util.GeneralUtil;
 import ge.isbu.demo.dto.AddEmployee;
 import ge.isbu.demo.entities.Department;
 import ge.isbu.demo.entities.Employee;
 import ge.isbu.demo.repositories.EmployeeRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +28,15 @@ public class EmployeeService {
         return employeeRepository.findById(id).orElseThrow(() -> new Exception("RECORD_NOT_FOUND"));
     }
 
+    @Transactional
     public Employee add(AddEmployee addEmployee) throws Exception {
         Employee employee = new Employee();
-        employee.setFirstName(addEmployee.getFirstName());
-        employee.setLastName(addEmployee.getLastName());
-        employee.setMiddleName(addEmployee.getMiddleName());
-        employee.setHireDate(addEmployee.getHireDate());
-        employee.setSalary(addEmployee.getSalary());
+//        employee.setFirstName(addEmployee.getFirstName());
+//        employee.setLastName(addEmployee.getLastName());
+//        employee.setMiddleName(addEmployee.getMiddleName());
+//        employee.setHireDate(addEmployee.getHireDate());
+//        employee.setSalary(addEmployee.getSalary());
+        GeneralUtil.getCopyOf(addEmployee, employee);
 
         Department department = departmentService.getById(addEmployee.getDepartmentId());
 
@@ -40,4 +44,15 @@ public class EmployeeService {
         return employeeRepository.save(employee);
     }
 
+    @Transactional
+    public Employee edit(Long id, AddEmployee addEmployee) throws Exception {
+            Employee employee = employeeRepository.findById(id).orElseThrow(() -> new Exception("EMPLOYEE_NOT_FOUND"));
+            //update employee data
+            GeneralUtil.getCopyOf(addEmployee, employee);
+            if (addEmployee.getDepartmentId() != null && !addEmployee.getDepartmentId().equals(employee.getDepartment().getDepartmentId())) {
+                Department department = departmentService.getById(addEmployee.getDepartmentId());
+                employee.setDepartment(department);
+            }
+            return employeeRepository.save(employee);
+    }
 }
